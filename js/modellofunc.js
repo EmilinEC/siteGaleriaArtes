213fd1153713969e722a26042883c89009a290c3
EmilinEC <emilinadamdalton@gmail.com>
2023-06-12 20:13:29 -0300

    inicio TrabDD

---
+++ js/modellofunc.js
@@ -0,0 +1,8 @@
+function MostraItemSelecionado() {
+  let item = document.getElementBiId("lista").selectedIndex; //mostra qual item que vc selecionou
+  if (item>0) {
+alert("Você selecionou o" +item + "° item da lista")
+
+  }
+
+}
